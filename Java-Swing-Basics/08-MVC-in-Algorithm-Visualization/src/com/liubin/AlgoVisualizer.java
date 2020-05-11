2e1966a254d62794e8a54188452a0161d6f3214c
liubin <870005335@qq.com>
2020-05-11 17:23:43 +0800

    算法可视化中的MVC

---
+++ Java-Swing-Basics/08-MVC-in-Algorithm-Visualization/src/com/liubin/AlgoVisualizer.java
@@ -0,0 +1,48 @@
+package com.liubin;
+
+import java.awt.*;
+
+/**
+ * @ClassName AlgoVisualizer
+ * @Description:
+ * @Author liubin
+ * @Date 2020/5/11 0011
+ * @Version V1.0
+ **/
+public class AlgoVisualizer {
+
+    private Circle[] circles;
+
+    private AlgoFrame algoFrame;
+
+    public AlgoVisualizer(int sceneWidth, int sceneHeight, int N) {
+        // 初始化数据
+        circles = new Circle[N];
+        int R = 50;
+        for (int i = 0; i < N; i++) {
+            int x = (int) (Math.random() * (sceneWidth - 2 * R)) + R;
+            int y = (int) (Math.random() * (sceneHeight - 2 * R)) + R;
+            int vx = (int) (Math.random() * 11) - 5;
+            int vy = (int) (Math.random() * 11) - 5;
+            circles[i] = new Circle(x, y, R, vx, vy);
+        }
+        // 初始化视图
+        EventQueue.invokeLater(() -> {
+            algoFrame = new AlgoFrame("Welcome", sceneWidth, sceneHeight);
+            new Thread(this::run).start();
+
+        });
+    }
+
+    private void run() {
+        while (true) {
+            // 绘制数据
+            algoFrame.render(circles);
+            AlgoVisHelper.pause(20);
+            // 更新数据
+            for (Circle circle : circles) {
+                circle.move();
+            }
+        }
+    }
+}
---
+++ Java-Swing-Basics/08-MVC-in-Algorithm-Visualization/src/com/liubin/Main.java
@@ -0,0 +1,18 @@
+package com.liubin;
+
+/**
+ * @ClassName Main
+ * @Description:
+ * @Author liubin
+ * @Date 2020/5/11 0011
+ * @Version V1.0
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        int sceneWidth = 800;
+        int sceneHeight = 800;
+        int N = 10;
+        AlgoVisualizer visualizer = new AlgoVisualizer(sceneWidth, sceneHeight, N);
+    }
+}
