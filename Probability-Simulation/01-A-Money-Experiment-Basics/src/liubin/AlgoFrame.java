2f5a59b3a5393b33cbb3f4b27c847d27aa5f2174
liubin <870005335@qq.com>
2020-05-12 11:28:39 +0800

    分钱问题基础

---
+++ Probability-Simulation/01-A-Money-Experiment-Basics/src/liubin/AlgoFrame.java
@@ -0,0 +1,82 @@
+package liubin;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @ClassName AlgoFrame
+ * @Description: 算法可视化视图层
+ * @Author liubin
+ * @Date 2020/5/11 0011
+ * @Version V1.0
+ **/
+public class AlgoFrame extends JFrame {
+
+    private int canvasWidth;
+
+    private int canvasHeight;
+
+    /**
+     *  构造器
+     * */
+    public AlgoFrame(String title, int canvasWidth, int canvasHeight) {
+        super(title);
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        // 使用画布
+        AlgoCanvas canvas = new AlgoCanvas();
+        // 设置画布大小
+        canvas.setPreferredSize(new Dimension(canvasWidth, canvasHeight));
+        // 设置内容面板
+        this.setContentPane(canvas);
+        // 调用pack方法调整窗口大小
+        pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public int getCanvasWidth() {
+        return canvasWidth;
+    }
+
+    public int getCanvasHeight() {
+        return canvasHeight;
+    }
+
+    // 设置自己的数据
+    private int[] money;
+    public void render(int[] money) {
+        this.money = money;
+        repaint();
+    }
+
+    private class AlgoCanvas extends JPanel {
+        public AlgoCanvas() {
+            // 双缓存
+            super(true);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            // 转换为Graphics2D对象
+            Graphics2D graphics2D = (Graphics2D) g;
+            // 开启抗锯齿
+            RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            graphics2D.addRenderingHints(hints);
+            // 具体绘制
+            AlgoVisHelper.setColor(graphics2D, AlgoVisHelper.Blue);
+            int w = canvasWidth / money.length;
+            for (int i = 0; i < money.length; i++) {
+                AlgoVisHelper.fillRectangle(graphics2D, i * w + 1, canvasHeight - money[i]
+                , w - 1, money[i]);
+            }
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(canvasWidth, canvasHeight);
+        }
+    }
+}
---
+++ Probability-Simulation/01-A-Money-Experiment-Basics/src/liubin/AlgoVisualizer.java
@@ -0,0 +1,59 @@
+package liubin;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.MouseAdapter;
+import java.util.Arrays;
+
+
+/**
+ * @ClassName AlgoVisualizer
+ * @Description: 算法可视化控制层
+ * @Author liubin
+ * @Date 2020/5/11 0011
+ * @Version V1.0
+ **/
+public class AlgoVisualizer {
+
+    // 创建自己的数据
+    private int[] money; // 数据
+
+    private AlgoFrame frame; // 视图
+
+    public AlgoVisualizer(String title, int sceneWidth, int sceneHeight) {
+        // 初始化数据
+        money = new int[100];
+        Arrays.fill(money, 100);
+
+        // 初始化视图
+        EventQueue.invokeLater(() -> {
+            frame = new AlgoFrame(title, sceneWidth, sceneHeight);
+            new Thread(this::run).start();
+        });
+    }
+
+
+    private void run() {
+        while (true) {
+            // 编写自己的动画逻辑
+            frame.render(money);
+            int delay = 10;
+            AlgoVisHelper.pause(delay);
+            // 更新数据
+            for (int i = 0; i < money.length; i++) {
+                if (money[i] > 0) {
+                    int j = (int) (Math.random() * money.length);
+                    money[i] -= 1;
+                    money[j] += 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int sceneWidth = 1000;
+        int sceneHeight = 500;
+        String title = "A-Money-Experiment-Basics";
+        AlgoVisualizer visualizer = new AlgoVisualizer(title, sceneWidth, sceneHeight);
+    }
+}
